a6550b9df6248a207821a357ae1804405e8d2f3e
steve.oh <steve.oh@absnewyork.com>
2021-02-09 17:30:51 -0500

    02-3 가변, 불변 변수

---
+++ src/02/02-4.js
@@ -0,0 +1,28 @@
+let num = 1;
+num = num * 3;
+
+let str = '문자';
+str = '다른 문자';
+
+let arr = [];
+arr = [1, 2, 3];
+
+let obj = {};
+obj = { name: '새 객체' };
+
+// const num = 1;
+// num = 3;
+
+// const str = '문자';
+// str = '새 문자';
+
+// const arr = [];
+// arr = [1, 2, 3];
+
+// const obj = {};
+// obj = { name: '내 이름' };
+
+const arr2 = [];
+arr2.push(1);
+arr.splice(0, 0, 0);
+arr.pop();
